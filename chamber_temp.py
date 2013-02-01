5f0cc139b36a937efb37faedf49ebad47f54c75b
ondrej <ondrejh.ck@gmail.com>
2013-02-01 13:00:53 +0100

    attempting to add start/stop functionality - need to be tested

--- chamber_temp.py
+++ chamber_temp.py
@@ -21,10 +21,16 @@ def parse_input():
 		sys.exit(-1)
 	return options.temp
 
-def set_temp(temp):
+def set_temp(temp,status=None):
 	# sets the temperature as temp 
 	ser = serial.Serial(portname, 9600, timeout=timeout)
-	nominal_temp_string = '${:02X}E {}\r\n'.format(address,temp)
+	if status=='start':
+		nominal_temp_string = '${:02X}E {:4.1f} 80.0 27.5 0.0 27.0 -191.3 0.0 -191.3 25.5 -191.3 0111000000000000\r\n'.format(address,temp)
+	elif status=='stop':
+		nominal_temp_string = '${:02X}E {:4.1f} 80.0 27.5 0.0 27.0 -191.3 0.0 -191.3 25.5 -191.3 0000000000000000\r\n'.format(address,temp)
+	else:
+		nominal_temp_string = '${:02X}E {:4.1f}\r\n'.format(address,temp)
+	#print(nominal_temp_string)
 	ser.write(nominal_temp_string.encode('ascii'))
 	ser.readlines() #flush port (ack should be there)
 	
@@ -41,11 +47,14 @@ def set_temp(temp):
 		print('Error in setting temperature. Please check the setup')
 
 if __name__=="__main__":
-	#temp = parse_input()
-	temp=35.0
-	#try:
-	set_temp(temp)
-	#except:
-	#	print('Error in setting temperature. Please check the setup')
+	'''temp = parse_input()
+	try:
+                set_temp(temp)
+	except:
+		print('Error in setting temperature. Please check the setup')
+	'''
+	#set_temp(35.0)
+	#set_temp(35.0,'start')
+	set_temp(35.0,'stop')
 		  
 
